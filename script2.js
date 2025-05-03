cd6676b7275e5770e7d24a007b15a2987d9ec3fa
Antant-Boutique <antant.boutique@gmail.com>
2025-05-03 17:17:10 +0530

    uploaded from wsl2

--- script2.js
+++ script2.js
@@ -14,8 +14,7 @@ const orderID = params.get('orderID');
 const container = document.getElementById('orderList');
 
 if (orderID) {
-  const firebaseURL = 'https://storage.googleapis.com/test1-1e3d0.appspot.com/orders/${orderID}.json?alt=media';
-  //const firebaseURL = 'https://firebasestorage.googleapis.com/v0/b/test1-1e3d0.appspot.com/o/orders/05022025-1.json?alt=media';
+  const firebaseURL = `https://storage.googleapis.com/test1-1e3d0.appspot.com/orders/${orderID}.json?alt=media`;
 
   fetch(firebaseURL)
     .then(response => {
